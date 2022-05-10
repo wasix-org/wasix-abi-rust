c48c1dc0989ab5127257abb877ae37fc2f01efff
Johnathan Sharratt <johnathan.sharratt@gmail.com>
2022-05-10 19:12:24 +0200

    Fixed the build so that it points to the right WASIX witx

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash -e
-cargo run -p witx-bindgen -- ../wasi-witx/phases/snapshot/witx/wasix_snapshot_preview1.witx > src/lib_generated.rs
+cargo run -p witx-bindgen -- ./crates/witx-bindgen/WASI/phases/snapshot/witx/wasix_snapshot_preview1.witx > src/lib_generated.rs
