05ab6b8ced0c21d5e64840f0965753b5dc96bfc2
Johnathan Sharratt <johnathan.sharratt@gmail.com>
2022-05-28 13:48:53 +0200

    Moved the WASI branch forward

--- build.sh
+++ build.sh
@@ -1,2 +1,6 @@
 #!/bin/bash -e
+git submodule update --init
+cd crates/witx-bindgen/WASI
+git pull origin main
+cd ../../..
 cargo run -p witx-bindgen -- ./crates/witx-bindgen/WASI/phases/snapshot/witx/wasix_snapshot_preview1.witx > src/lib_generated.rs
