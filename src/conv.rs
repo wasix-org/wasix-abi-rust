0da1a67416bc336dbeaf1c54b53123f64c2965c0
Johnathan Sharratt <johnathan.sharratt@gmail.com>
2023-05-19 13:55:16 +1000

    Added more conversion functions

--- src/conv.rs
+++ src/conv.rs
@@ -3,21 +3,31 @@ use super::x as x;
 impl From<wasi::Errno>
 for x::Errno {
     fn from(value: wasi::Errno) -> Self {
-       value.raw().into()
+        unsafe { core::mem::transmute(value) }
     }
 }
 
 impl From<wasi::Filetype>
 for x::Filetype {
     fn from(value: wasi::Filetype) -> Self {
-       value.raw().into()
+        unsafe { core::mem::transmute(value) }
     }
 }
 
-/// Unsafe
-///
-/// The Errno will be exactly the same size and should be compatible
-///
+impl From<wasi::Ciovec>
+for x::Ciovec {
+    fn from(value: wasi::Ciovec) -> Self {
+        unsafe { core::mem::transmute(value) }
+    }
+}
+
+impl From<wasi::Iovec>
+for x::Iovec {
+    fn from(value: wasi::Iovec) -> Self {
+        unsafe { core::mem::transmute(value) }
+    }
+}
+
 impl From<x::Errno>
 for wasi::Errno {
     fn from(value: x::Errno) -> Self {
@@ -25,13 +35,23 @@ for wasi::Errno {
     }
 }
 
-/// Unsafe
-///
-/// The Errno will be exactly the same size and should be compatible
-///
 impl From<x::Filetype>
 for wasi::Filetype {
     fn from(value: x::Filetype) -> Self {
+        unsafe { core::mem::transmute(value) }
+    }
+}
+
+impl From<x::Ciovec>
+for wasi::Ciovec {
+    fn from(value: x::Ciovec) -> Self {
+        unsafe { core::mem::transmute(value) }
+    }
+}
+
+impl From<x::Iovec>
+for wasi::Iovec {
+    fn from(value: x::Iovec) -> Self {
         unsafe { core::mem::transmute(value) }
     }
 }
